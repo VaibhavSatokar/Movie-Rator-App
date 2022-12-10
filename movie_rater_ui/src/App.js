08bc9e4ac419efa8aca155cc00b74081588827ec
Vaibhav Satokar <v.satokar@khscience.in>
2022-12-10 12:15:55 +0530

    List Component

--- movie_rater_ui/src/App.js
+++ movie_rater_ui/src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import Header from './components/header/header';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const[movies, setMovies] = useState(["Movie1","Movie2"])
+
   return (
     <div className="App">
       <Header appName="Movie Rater" />
       <div className='layout'>
-        <div>Movie List</div>
-        <div>Movie Details</div>
+        <div>
+          {movies.map(movie=>{
+            return <h1>{movie}</h1>
+          })}
+        </div>
+
+        <div>
+          Movie Details
+        </div>
       </div>
     </div>
   );
