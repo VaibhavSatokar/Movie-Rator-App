b890c9ed58414b90d4b83acc8c1819a61da94a6d
Kh Infinite Possibilities Pvt. Ltd <v.satokar@khscience.in>
2022-12-10 18:54:52 +0530

    Rating stars

--- movie_rater_ui/src/components/movies/movie-details.js
+++ movie_rater_ui/src/components/movies/movie-details.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 
 function MovieDetails(props) {
+
+    const[highlighted, setHighlighted] = useState(-1)
+
+    const highlighteRate = high =>evt =>{
+        setHighlighted(high);
+    }
+
   return (
     <div className='movie-details'>
         {props.movie ? (
@@ -16,6 +23,18 @@ function MovieDetails(props) {
                 <FontAwesomeIcon icon={faStar} className={props.movie.avg_rating > 3 ? 'orange' : ''} />
                 <FontAwesomeIcon icon={faStar} className={props.movie.avg_rating > 4 ? 'orange' : ''}/>
                 ({props.movie.no_of_ratings})
+
+                <div className='rate-container'>
+                    <h2>Rate it</h2>
+                    {[...Array(5)].map((e, i)=>{
+                        return <FontAwesomeIcon key={i} icon={faStar} className={highlighted > i-1 ? 'purple' : ''}
+                        onMouseEnter = {highlighteRate(i)}
+                        onMouseLeave = {highlighteRate(-1)}
+                        />
+                    })
+                    }
+                </div>
+
             </div>
         ) : null}
     </div>
