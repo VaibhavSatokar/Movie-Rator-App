82c9ac4412ad563cd6ff41d5202587ab2425cf25
Kh Infinite Possibilities Pvt. Ltd <v.satokar@khscience.in>
2022-12-12 18:08:18 +0530

    Edit Component Form

--- movie_rater_ui/src/components/movies/movie-form.js
+++ movie_rater_ui/src/components/movies/movie-form.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 function MovieForm(props) {
+    const [title, setTitle] = useState(props.movie.title);
+    const [description, setDescription] = useState(props.movie.description);
+
+    const updateClicked = () =>{
+        console.log('Update Clicked')
+    }
+
   return (
     <React.Fragment>
         {props.movie ? (
             <div className='movie-form'>
-                <label htmlFor='title'>Title</label>
-                <input type='text' id='movie-title' placeholder='Title' />
-                <label htmlFor='description'>Description</label>
-                <input type='textarea' id='movie-description' placeholder='Description'/>
+                <label htmlFor='title'>Title</label><br/>
+                <input type='text' id='title' placeholder='Title' 
+                value={title} onChange={evt=>setTitle(evt.target.value)}
+                /><br/>
+                <label htmlFor='description'>Description</label><br/>
+                <textarea type='text' id='description' placeholder='Description'
+                value={description} onChange={evt=>setDescription(evt.target.value)}
+                ></textarea><br/>
+                <button type='submit' onClick={updateClicked}>Update</button>
             </div>
         ) : null}
     </React.Fragment>
