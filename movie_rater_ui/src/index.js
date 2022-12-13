b9f320c43844c293395420f02a7f410a80836df4
Kh Infinite Possibilities Pvt. Ltd <v.satokar@khscience.in>
2022-12-13 19:29:19 +0530

    Cookies

--- movie_rater_ui/src/index.js
+++ movie_rater_ui/src/index.js
@@ -1,44 +1,47 @@
-import React, {useState, createContext } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {CookiesProvider} from 'react-cookie'
+import reportWebVitals from './reportWebVitals'
+import {Route, BrowserRouter, Routes} from 'react-router-dom'
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Auth from './components/auth/auth'
 
-import {Route, BrowserRouter, Routes} from 'react-router-dom'
-import Auth from './components/auth/auth';
 
 /* const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Auth/>}/>
-      <Route path='/movies' element={<App/>}/>
-    </Routes>
-    </BrowserRouter>
+    <CookiesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<Auth/>}/>
+          <Route exact path='/movies' element={<App/>}/>
+        </Routes>
+      </BrowserRouter>
+    </CookiesProvider>
   </React.StrictMode>
-);  */
+);   */
 
-export const TokenContext = createContext(null)
-
-function Router(){
-  const [token, setToken] = useState('');
+ function Router(){
   return(
-    <TokenContext.Provider value={{token, setToken}}>
-      <React.StrictMode>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Auth/>}/>
-            <Route path='/movies' element={<App/>}/>
-          </Routes>
-        </BrowserRouter>
-    </React.StrictMode>
-    </TokenContext.Provider>
+  <React.StrictMode>
+    <CookiesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<Auth/>}/>
+          <Route exact path='/movies' element={<App/>}/>
+        </Routes>
+      </BrowserRouter>
+    </CookiesProvider>
+  </React.StrictMode>
   )
-};
+}; 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Router />);
+root.render(<Router />)
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
