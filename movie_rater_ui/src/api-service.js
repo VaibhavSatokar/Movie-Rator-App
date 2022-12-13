c3f88a77013eec7e21fd7cdb0a3c8b3117902de7
Kh Infinite Possibilities Pvt. Ltd <v.satokar@khscience.in>
2022-12-13 16:09:23 +0530

    Login Page

--- movie_rater_ui/src/api-service.js
+++ movie_rater_ui/src/api-service.js
@@ -1,7 +1,15 @@
 const TOKEN = "625053af7b8ec3c1711c8002e14054b3a2f0263c"
 
 export default class API {
-  
+
+    static signInUser(body){
+        return fetch(`http://127.0.0.1:8000/auth/`, {
+            method: 'POST',
+            headers: {'Content-Type':'application/json'},
+            body: JSON.stringify(body) 
+        }).then(response => response.json());
+    }
+
     static updateMovie(movie_id, body){
         return fetch(`http://127.0.0.1:8000/api/movies/${movie_id}/`,{
             method:'PUT',
--- movie_rater_ui/src/components/auth/auth.js
+++ movie_rater_ui/src/components/auth/auth.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import API from '../../api-service';
 
 function Auth() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signInClicked = () =>{
+        API.signInUser({username,password})
+        .then(resp => console.log(resp))
+        .catch(error => console.log(error));
+    }
+
+
   return (
-    <h1>AUTH</h1>
+    <div>
+        <label htmlFor="username">Username</label> <br/>
+        <input id="username" type="text" placeholder='username' value={username} 
+        onChange={ evt => setUsername(evt.target.value)}/>
+        <br/>
+
+        <label htmlFor="password">Password</label><br/>
+        <input id="password" type="password" placeholder='password' value={password} 
+        onChange={ evt => setPassword(evt.target.value)}/>
+        <br/>
+
+        <button type='submit' onClick={signInClicked}>Sign In</button>     
+    </div>
   )
 }
 
